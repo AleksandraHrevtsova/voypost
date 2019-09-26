b954560ea5949540280a63b02430be9dcef38a59
aleksandra.hrevtsova <aleksandra.hrevtsova@gmail.com>
2019-09-26 10:42:11 +0300

    Updates

---
+++ precache-manifest.6ccbbd5b53e738b5bb14097be314e1cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975edd946378d68b612f69dcdab4e781",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dea83922559487e84ce",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "3ea6fe646fe653b82182",
+    "url": "/static/js/2.0de31dcc.chunk.js"
+  },
+  {
+    "revision": "3dea83922559487e84ce",
+    "url": "/static/js/main.854d0dca.chunk.js"
+  },
+  {
+    "revision": "bd331fb02e15c62dc8d6",
+    "url": "/static/js/runtime-main.927ecb10.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
